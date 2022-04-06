994bb369851e5a28fabcc12b85868a087ac71e25
Jonas Wijne <jonas.wijne@jimber.io>
2022-04-06 16:46:09 +0200

    eslint add @typescript-eslint plugin

--- packages/config/eslint-vue-ts-preset.js
+++ packages/config/eslint-vue-ts-preset.js
@@ -2,14 +2,19 @@ module.exports = {
     env: {
         node: true,
     },
+    plugins: [
+        "@typescript-eslint"
+    ],
     globals: {
         defineProps: 'readonly',
         defineEmits: 'readonly',
         withDefaults: 'readonly',
     },
     extends: [
-        '@vue/typescript/recommended',
         'eslint:recommended',
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        '@vue/typescript/recommended',
         'plugin:vue/vue3-recommended',
         'prettier',
         'plugin:typescript-enum/recommended',
@@ -18,5 +23,11 @@ module.exports = {
     rules: {
         'vue/multi-word-component-names': 'off',
         'vue/no-setup-props-destructure': 'off',
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-ignore': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
     },
 };
