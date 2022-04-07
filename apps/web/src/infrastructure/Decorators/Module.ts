c06d8aed12c877fa3050546239b0d1279e26b48b
Jonas Wijne <jonas.wijne@jimber.io>
2022-04-07 21:07:35 +0200

    fix lint

--- apps/web/src/infrastructure/Decorators/Module.ts
+++ apps/web/src/infrastructure/Decorators/Module.ts
@@ -1,8 +1,5 @@
-import {InitUiCommandHandler} from "@/modules/ui/CommandHandlers/InitUiCommandHandler";
-
 export const Module = (param: { provides: any[] }) => {
     return (target: any) => {
         target.prototype.provides = param.provides;
     };
 };
-
